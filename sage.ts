8d7664886ef77b1459b88e0e1e1c5be2657139b7
AptCoding <shankaruday443@gmail.com>
2025-03-16 20:56:06 +0000

    minor cahnges

--- sage.ts
+++ sage.ts
@@ -2,11 +2,25 @@ import axios from "axios";
 import "dotenv/config";
 import { createClient } from "@libsql/client";
 
+const tursoUrl = process.env.TURSO_DATABASE_URL;
+const tursoAuthToken = process.env.TURSO_AUTH_TOKEN;
+
+if (!tursoUrl || !tursoAuthToken) {
+  throw new Error(
+    "TURSO_URL and TURSO_AUTH_TOKEN must be defined in the environment variables"
+  );
+}
+
 export const turso = createClient({
-  url: "libsql://connect-rolecservices.turso.io",
-  authToken:
-    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzM4MzQ0OTcsImlkIjoiNDI4MjZkMTktZWY1Ny00OGIyLWI3OTEtNmVmYzFkMTk3M2U5In0.sZgED9XH18pfnIHUBpmbPGV7WHXztmGf9Yq8yOSRFldYyiJGYoEimfDf8hMvrhtDH5hnIivuCyO_1tSsh27WAQ",
+  url: tursoUrl,
+  authToken: tursoAuthToken,
 });
+
+if (!tursoUrl || !tursoAuthToken) {
+  throw new Error(
+    "TURSO_URL and TURSO_AUTH_TOKEN must be defined in the environment variables"
+  );
+}
 
 export const getData = async () => {
   try {
@@ -29,7 +43,7 @@ export const getData = async () => {
 async function checkAndReturnPushNotificationToken(installerID: string) {
   try {
     const tursoData = await turso.execute(`
-      SELECT sage_person_id, 
+      SELECT sage_person_id
       FROM electrician
       JOIN external_type ON electrician.uuid= external_type.electrician_uuid
       JOIN external ON external_type.external_uuid = external.uuid
